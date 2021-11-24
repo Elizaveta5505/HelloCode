f4c23a7961ce5994e20487a3c8483299ee1d2d28
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-24 23:39:47 +0300

    save changes

---
+++ Seminar2/Program.cs
@@ -0,0 +1,19 @@
+﻿int number1 = 13;
+int number2 = 8;
+int max = number1;
+int min = number2;
+
+if (number1 > number2)
+{
+    max = number1;
+    min = number2;
+    Console.WriteLine(max);
+    Console.WriteLine(min);
+}
+else
+{
+    max = number2;
+    min = number1;
+    Console.WriteLine(min);
+    Console.WriteLine(max);
+}
---
+++ Seminar3/Program.cs
@@ -0,0 +1,9 @@
+﻿int number1 = Monday;
+int number2 = Tuesday;
+int number3 = Wednesday;
+int number4 = Thursday;
+int number5 = Friday;
+int number6 = Saturday;
+int number7 = Sunday;
+Console.Write("Введите номер дня недели: ");
+string number = Console.ReadLine();
