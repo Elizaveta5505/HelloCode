fcce42a204709829b4064c209953aa6ff39750ec
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-24 23:59:07 +0300

    example3

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,9 +1,9 @@
-﻿int number1 = Monday;
-int number2 = Tuesday;
-int number3 = Wednesday;
-int number4 = Thursday;
-int number5 = Friday;
-int number6 = Saturday;
-int number7 = Sunday;
-Console.Write("Введите номер дня недели: ");
+﻿Console.Write("Введите номер дня недели: ");
 string number = Console.ReadLine();
+if (number == "1") Console.WriteLine("Понедельник. День тяжёлый");
+if (number == "2") Console.WriteLine("Вторник");
+if (number == "3") Console.WriteLine("Среда. Маленькая пятница");
+if (number == "4") Console.WriteLine("Четверг");
+if (number == "5") Console.WriteLine("Пятница - развратница");
+if (number == "6") Console.WriteLine("Суббота");
+if (number == "7") Console.WriteLine("Воскресенье - выходной!");
