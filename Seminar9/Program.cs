9ca49822d03f0828a22c442d4756a61b7acc8231
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-25 22:00:58 +0300

    even numbers 1 to n

---
+++ Seminar9/Program.cs
@@ -0,0 +1,14 @@
+﻿// 8. Показать четные числа от 1 до N
+Console.Write ("Введите значение n: ");
+int n = int.Parse(Console.ReadLine());
+int a = 1;
+
+while (a<n)
+{
+     if (a%2 == 0)
+     {
+         Console.WriteLine(a++);
+         
+     }
+     a++;
+}
