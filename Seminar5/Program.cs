3f12fad6e8c7d8febd1b07fb337ee9bf49af7122
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-25 21:57:49 +0300

    programm for y=f(a)

---
+++ Seminar5/Program.cs
@@ -0,0 +1,11 @@
+﻿// 6. Выяснить является ли число чётным
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
