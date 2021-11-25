6a97a46e96090d3d6f42fda6552c5d7df06bd43a
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-25 22:00:13 +0300

    even numbers 1 to n

---
+++ Seminar8/Program.cs
@@ -0,0 +1,6 @@
+﻿// 7. Показать числа от -N до N
+Console.WriteLine("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+int [] array = {-N, +N };
+int n = array.Lenght;
+Console. WriteLine(array);
