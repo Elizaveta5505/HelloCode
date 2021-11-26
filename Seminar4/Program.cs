a19a32e7798dc3ad451162c7e0da23fbbea9c3bc
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-26 19:10:01 +0300

    find max - 3 figure

---
+++ Seminar4/Program.cs
@@ -0,0 +1,12 @@
+﻿// 4. Найти максимальное из трех чисел
+int A = 1;
+int B = 77;
+int C = 9;
+int max = C;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+
+Console.Write("max = ");
+Console.WriteLine(max);
