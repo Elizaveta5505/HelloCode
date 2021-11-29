1c25070e5b24c65dca8500a18cc954ca673406c0
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-29 22:20:27 +0300

    third figure else no number

--- Seminar15/Program.cs
+++ Seminar15/Program.cs
@@ -1,1 +1,11 @@
 ﻿// 14. Найти третью цифру числа или сообщить, что её нет
+int number = int.Parse(Console.ReadLine());
+string num = number.ToString();
+if (number> 99)
+{
+    Console.WriteLine(num[2]);
+}
+else 
+{
+    Console.WriteLine("no number");
+}
