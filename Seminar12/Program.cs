abda8dc2ff47bb197f45bc09d958b4845157372b
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-29 18:02:57 +0300

    bigger figure

--- Seminar12/Program.cs
+++ Seminar12/Program.cs
@@ -1,1 +1,11 @@
 ﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random().Next (10, 100);
+Console.WriteLine(number);
+if (number%10 > number/10) // если последняя цифра числа больше первой (%10 - показывает последнюю цифру числа, а /10 - показывает первые цифры числа)
+{
+    Console.WriteLine(number%10);
+}
+else 
+{
+    Console.WriteLine(number/10);
+}
