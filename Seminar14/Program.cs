7de66c1da2233a668702e73820985b105cc5357a
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-29 21:13:50 +0300

    remains

--- Seminar14/Program.cs
+++ Seminar14/Program.cs
@@ -1,1 +1,11 @@
 ﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток
+int number = int.Parse(Console.ReadLine());
+int numA = 5;
+if(number/numA == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine(number%numA);
+}
