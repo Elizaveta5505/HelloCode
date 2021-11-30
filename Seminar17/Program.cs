0bd9e7524a75260e697b90393f84397c9d138d83
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-30 23:06:13 +0300

    day week

--- Seminar17/Program.cs
+++ Seminar17/Program.cs
@@ -1,3 +1,11 @@
 ﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
 Console.WriteLine("Введите число");
-int C = int.Parse(WriteLine());
+int C = int.Parse(Console.ReadLine());
+if (C>5)
+{
+    Console.WriteLine("Выходной!");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
