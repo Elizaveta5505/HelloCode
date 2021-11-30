ebdb798dcfa86f8f56bee03d254ee140212ecb73
Elizaveta <al5xeevaliz@yandex.ru>
2021-11-30 19:31:41 +0300

    7 and 23

--- Seminar16/Program.cs
+++ Seminar16/Program.cs
@@ -1,1 +1,7 @@
 ﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Введите число");
+int number = int.Parse(ConsoleWriteLine());
+for ( number%7 == 0; number%23 == 0 )
+{
+    Console.WriteLine("Кратно");
+}
