7c09b8a3b0d716fe1e676aa18fe0a8c8eee3c1d9
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-06 22:01:54 +0300

    quarter

---
+++ Seminar21/Program.cs
@@ -0,0 +1,8 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+if (quarter == 1) Console.WriteLine("X > 0, Y > 0");
+if (quarter == 2) Console.WriteLine("X < 0, Y > 0");
+if (quarter == 3) Console.WriteLine("X < 0, Y < 0");
+if (quarter == 4) Console.WriteLine("X > 0, Y < 0");
+if (quarter > 4) Console.WriteLine("Такой четверти нет");
