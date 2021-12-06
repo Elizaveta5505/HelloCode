49f7f1baf3c9c541fc3c201f8b6a468c4b9eab51
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-06 22:02:57 +0300

    2D & 3D

---
+++ Seminar23/Program.cs
@@ -0,0 +1,11 @@
+﻿// 22. Найти расстояние между точками в пространстве 2D/3D
+int[] A = {3, 8};
+int[] B = {5, 6};
+int result = 0;
+for (int i = 0; i < A.Length; i++)
+{
+    for (int j = 0; j < B.Length; j++)
+    {
+        Console.WriteLine(result == (result + (B[j] - A[i])*(B[j] - A[i])));
+    }
+}
