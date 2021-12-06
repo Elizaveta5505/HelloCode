48c072e67b24b5190f76695450d048b141cf6fc0
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-06 22:01:15 +0300

    true or false

--- Seminar19/Program.cs
+++ Seminar19/Program.cs
@@ -5,12 +5,14 @@
     bool[] y = {true, false};
     for (int i = 0; i < x.Length; i++)
     {
-        Console.Write("Для x: ");
-        Console.WriteLine (x[i]);
+        
         for (int n = 0; n < y.Length; n++)
         {
+            Console.Write("Для x: ");
+            Console.WriteLine (x[i]);
             Console.Write("Для y: ");
             Console.WriteLine (y[n]);
+
             if (!(x[i] || y[n]) == (!x[i] && !y[n]))
             {
               Console.WriteLine("Выражение истинно");
