bd47efe15a476f2cd66c376ff465e4212596aab5
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-09 19:48:21 +0300

    table of squares

--- Seminar24/Program.cs
+++ Seminar24/Program.cs
@@ -1,1 +1,7 @@
 ﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+for (int num = 1; num <= N; num++)
+{
+    Console.WriteLine($"{num}^2 = " + Math.Pow(num, 2));
+}
