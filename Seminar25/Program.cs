d41694dadebc551ad0020c234dd24abc435d7ef2
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-09 19:48:51 +0300

    cube of numbers

--- Seminar25/Program.cs
+++ Seminar25/Program.cs
@@ -1,1 +1,7 @@
 ﻿// 24. Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+for (int num = 1; num <= N; num++)
+{
+    Console.WriteLine(Math.Pow(num, 3));
+}
