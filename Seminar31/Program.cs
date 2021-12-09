437a9cc6315ea9a9dcdd76054f0777db396bf0d6
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-09 19:55:27 +0300

    cube of even numbers

--- Seminar31/Program.cs
+++ Seminar31/Program.cs
@@ -1,1 +1,7 @@
 ﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+int number = new Random().Next (1, 10);
+Console.WriteLine(number);
+for (int i = 0; i < number; i++)
+{
+    if (Math.Pow(i, 3) %2 == 0) Console.WriteLine(Math.Pow(i, 3));
+}
