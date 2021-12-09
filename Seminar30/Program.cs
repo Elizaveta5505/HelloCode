69b0bd673d8f7a69a99a14ddcca43d4b16d16d23
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-09 19:54:50 +0300

    multiplication of numbers

--- Seminar30/Program.cs
+++ Seminar30/Program.cs
@@ -1,1 +1,10 @@
 ﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine());
+int mult = 1;
+for (int num = 1; num <= N; num++)
+{
+    mult = mult * num;
+}
+Console.Write ("Произведение чисел равно:");
+Console.WriteLine(mult);
