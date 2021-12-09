b7224f4ede34d81ac32ddc6c38b31706db2d6198
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-09 19:50:24 +0300

    sum of figure in a number

--- Seminar28/Program.cs
+++ Seminar28/Program.cs
@@ -1,1 +1,11 @@
 ﻿// 27. Определить количество цифр в числе
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int result = 0;
+while (number > 0)
+{
+    result++;
+    number = (number/10);
+}
+Console.Write ("Количество цифр в числе: ");
+Console.WriteLine(result);
