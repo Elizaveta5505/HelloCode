141fa29fa8a110bf391e4845742d9d7cdbc997b4
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-09 19:59:02 +0300

    even and odd numbers

--- Seminar37/Program.cs
+++ Seminar37/Program.cs
@@ -1,1 +1,43 @@
 ﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(99, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(arr[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+int even = 0;
+int odd = 0;
+FillArray(array);
+PrintArray(array);
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        even = even + 1;
+    }
+    else
+    {
+        odd = odd +1;
+    }
+}
+Console.Write("Количество чётных чисел: ");
+Console.WriteLine(even);
+Console.Write("Количество нечётных чисел: ");
+Console.WriteLine(odd);
