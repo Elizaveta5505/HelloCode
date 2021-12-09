10c02f0b2061f6b0bd67af9feccdf3fb20719322
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-09 19:49:48 +0300

    number of digits in a number

--- Seminar27/Program.cs
+++ Seminar27/Program.cs
@@ -1,1 +1,13 @@
 ﻿// 26. Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("Введите число A:");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень B:");
+int B = int.Parse(Console.ReadLine());
+if (B > 0) 
+{
+    Console.WriteLine(Math.Pow(A, B));
+}
+else
+{
+    Console.WriteLine("Степень не натуральная");
+}
