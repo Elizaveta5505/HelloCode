b0e2341e31fed8b4670710aa5eb1a6e65168e708
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-09 19:49:15 +0300

    sum of numbers

--- Seminar26/Program.cs
+++ Seminar26/Program.cs
@@ -1,1 +1,10 @@
 ﻿// 25. Найти сумму чисел от 1 до А
+Console.WriteLine("Введите число A:");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int num = 1; num <= A; num++)
+{
+    sum = sum + num;
+}
+Console.Write ("Сумма чисел равна:");
+Console.WriteLine(sum);
