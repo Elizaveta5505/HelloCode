977cf3a9f1d2f3a1afa1a5451bc69f93dc11c17f
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-13 22:58:32 +0300

    number bigger than 0

---
+++ Seminar43/Program.cs
@@ -0,0 +1,19 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+string num = number.ToString();
+int[] array = new int[num.Length];
+for(int i = 0; i < num.Length; i++)
+{
+    array[i] = int.Parse(num[i].ToString());
+}
+int count = 0;
+for(int j = 0; j < array.Length; j++)
+{
+    if (array[j] >= 1)
+    {
+        count = count + 1;
+    }
+}
+Console.Write ("Количество чисел больше 0: ");
+Console.WriteLine(count);
