57d796edb9e9ef08aa5627baf95de926207d717d
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-13 23:00:04 +0300

    intersection points

---
+++ Seminar45/Program.cs
@@ -0,0 +1,13 @@
+﻿// 44. Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы.
+Console.WriteLine("Введите число b1: ");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число k1: ");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b2: ");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число k2: ");
+int k2 = int.Parse(Console.ReadLine());
+double X = (b2 - b1)/(k1 - k2);
+double Y = k1 * X + b1;
+Console.WriteLine(X);
+Console.WriteLine(Y);
