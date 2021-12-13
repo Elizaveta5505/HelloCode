de3064cff402b2cc47384ab9ee52b55db39b3e82
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-13 22:54:48 +0300

    mass of 123 element [10, 99]

--- Seminar38/Program.cs
+++ Seminar38/Program.cs
@@ -1,1 +1,36 @@
 ﻿// 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(arr[position]);
+        position++;
+    }
+}
+
+int[] array = new int[123];
+FillArray(array);
+PrintArray(array);
+int elements = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 9 && array[i] < 100)
+    {
+        elements = elements + 1;
+    }
+}
+Console.Write("Количество элементов из отрезка: ");
+Console.WriteLine(elements);
