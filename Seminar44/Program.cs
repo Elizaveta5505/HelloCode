0d0a2856e65caac68aff0fe730ba37d16330c15d
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-13 22:59:37 +0300

    of decimal in binary

---
+++ Seminar44/Program.cs
@@ -0,0 +1,13 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+Console.Write("Введите дисятичное число: ");
+int decimalNumber = int.Parse(Console.ReadLine());
+int transformation;
+string result = string.Empty;
+while (decimalNumber > 0)
+{
+    transformation = decimalNumber % 2;
+    decimalNumber /= 2;
+    result = transformation.ToString() + result;
+}
+Console.Write("Двоичное число: ");
+Console.WriteLine(result);
