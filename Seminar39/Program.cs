1a2aa14af57a9e144539327adfedd61c0a04e566
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-13 22:55:34 +0300

    multiplying numbers in an array

--- Seminar39/Program.cs
+++ Seminar39/Program.cs
@@ -1,1 +1,33 @@
 ﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(arr[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+int sum = 0;
+for (int i = 1; i < array.Length; i = i + 2)
+{
+    sum = sum + array[i];
+}
+Console.Write("Сумма элементов на нечётной позиции: ");
+Console.WriteLine(sum);
