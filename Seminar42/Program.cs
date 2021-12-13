841c4092ebc50956bb0702bc132476e9bedc5fc6
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-13 22:58:00 +0300

    sides of the triangle

---
+++ Seminar42/Program.cs
@@ -0,0 +1,15 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника 
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c = int.Parse(Console.ReadLine());
+if (a + b > c && a + c > b && b + c > a)
+{
+    Console.WriteLine("Числа являются сторонами треугольника");
+}
+else
+{
+    Console.WriteLine("Числа не являются сторонами треугольника");
+}
