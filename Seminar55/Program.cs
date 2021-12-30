8fd2811e196f60eabc8522ea936cceee6830fcec
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:13:19 +0300

    elements the main diagonal

---
+++ Seminar55/Program.cs
@@ -0,0 +1,39 @@
+﻿// 54. В матрице чисел найти сумму элементов главной диагонали
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3, 3];
+FillArray(matrix);
+PrintArray(matrix);
+int sum = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + matrix[i,j];
+            }
+        }
+       
+    } 
+Console.WriteLine($"Сумма элементов главной диагонали равна: {sum}");
