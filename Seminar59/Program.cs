cc827b3236bbb2b71382ce0c73d600f2e83baadd
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:18:43 +0300

    replacing rows with columns

---
+++ Seminar59/Program.cs
@@ -0,0 +1,42 @@
+﻿// 58. Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, что это невозможно (в случае, если матрица не квадратная).
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void ExchangeArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int elements = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = elements;
+        }
+    }
+}
+
+int[,] matrix = new int[3, 3];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ExchangeArray(matrix);
+PrintArray(matrix);
