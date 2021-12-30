c31b00385459c960864ef6a4a3df7e7b1b7780a0
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:34:29 +0300

    functional Akkerman

---
+++ Seminar72/Program.cs
@@ -0,0 +1,9 @@
+﻿// 71. Написать программу вычисления функции Аккермана
+int functionAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return functionAkkerman(m - 1, 1);
+    if (m > 0 && n> 0) return functionAkkerman(m - 1, (functionAkkerman(m, n - 1)));
+    return 0;
+}
+Console.WriteLine(functionAkkerman(5, 5));
