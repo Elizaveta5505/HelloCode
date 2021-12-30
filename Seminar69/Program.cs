ccc1fe29c901dadd2b8c6894fa83d03b0b95021c
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:32:03 +0300

    N number on M to N

---
+++ Seminar69/Program.cs
@@ -0,0 +1,8 @@
+﻿// 68. Показать натуральные числа от M до N, N и M заданы
+string Number (int M, int N)
+{
+    if(M>N) return $"{M} " + Number(M-1, N);
+    else if (N>M) return $"{M} " + Number(M+1, N);
+    else return M.ToString();
+}
+Console.WriteLine(Number(1, 3));
