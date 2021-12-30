5c3d952b251c9e20c504b407bedb6f90e0ab6a74
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:07:28 +0300

    array nxk even number

---
+++ Seminar51/Program.cs
@@ -0,0 +1,38 @@
+﻿// 50. В двумерном массиве n×k заменить четные элементы на противоположные
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3,4];
+FillArray(matrix);
+PrintArray(matrix);
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i,j] % 2 == 0) 
+        {
+            matrix[i,j] = matrix[i,j] * (-1);
+        }
+    }
+}
+Console.WriteLine();
+PrintArray(matrix);
