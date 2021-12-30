d2cadf6738294c695b556226d0433632731065c6
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:16:14 +0300

    exchange of the first and last line

---
+++ Seminar57/Program.cs
@@ -0,0 +1,39 @@
+﻿// 56. Написать программу, которая обменивает элементы первой строки и последней строки
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void ExchangeArray(int[,] matrix)
+{
+    int size = matrix.GetLength(0);
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int elements = matrix[0,j];
+        matrix[0, j] = matrix[size - 1, j];
+        matrix[size - 1, j] = elements;
+    }
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ExchangeArray(matrix);
+PrintArray(matrix);
