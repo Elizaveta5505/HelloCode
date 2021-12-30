de101ffb351b4de8bf7ae278c5b552c4639a9b2c
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:09:57 +0300

    even index number on square

---
+++ Seminar53/Program.cs
@@ -0,0 +1,38 @@
+﻿// 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3,3];
+FillArray(matrix);
+PrintArray(matrix);
+ for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i,j] = matrix[i,j] * matrix[i,j];
+            }
+        }
+    }
+Console.WriteLine();
+PrintArray(matrix);
