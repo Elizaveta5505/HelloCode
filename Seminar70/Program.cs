f8621e6ce1b74b813080a61598d9862d1fe50158
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:32:50 +0300

    summ elements on M to N

---
+++ Seminar70/Program.cs
@@ -0,0 +1,18 @@
+﻿// 69. Найти сумму элементов от M до N, N и M заданы
+string Number(int M, int N)
+{
+    if (M > N) return $"{M} " + Number(M - 1, N);
+    else if (M < N) return $"{M} " + Number(M + 1, N);
+    else return M.ToString();
+}
+int Sum(int M, int N)
+{
+    if (M > N) return M + Sum(M - 1, N);
+    else if (M < N) return M + Sum(M + 1, N);
+    else return M;
+}
+
+Console.Write(Number(5, 1));
+Console.Write(" = ");
+Console.WriteLine(Sum(5, 1));
+
