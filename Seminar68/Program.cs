68375e0416fd800fc20a951211924ea86e593eff
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:31:40 +0300

    N number on N to 1

---
+++ Seminar68/Program.cs
@@ -0,0 +1,7 @@
+﻿// 67. Показать натуральные числа от N до 1, N задано
+string Number (int a)
+{
+    if(a>=1) return $"{a} " + Number(a-1);
+    else return String.Empty;
+}
+Console.WriteLine(Number(10));
