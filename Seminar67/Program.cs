d5e24624e23f8f1953aeb426ca86328df1f2a992
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-30 19:31:17 +0300

    N number on 1 to N

---
+++ Seminar67/Program.cs
@@ -0,0 +1,7 @@
+﻿// 66. Показать натуральные числа от 1 до N, N задано
+string Number (int a)
+{
+    if(a<=10) return $"{a} " + Number(a+1);
+    else return String.Empty;
+}
+Console.WriteLine(Number(1));
